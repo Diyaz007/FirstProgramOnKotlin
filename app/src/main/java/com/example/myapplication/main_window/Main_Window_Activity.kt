4c15c5ca75fe1a01ab8c0c45bd647a7fafac4730
Diyaz007 <diyaz18.08@gmail.com>
2023-12-02 02:01:00 +0600

    add logout

--- app/src/main/java/com/example/myapplication/main_window/Main_Window_Activity.kt
+++ app/src/main/java/com/example/myapplication/main_window/Main_Window_Activity.kt
@@ -1,12 +1,22 @@
 package com.example.myapplication.main_window
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.myapplication.R
+import com.example.myapplication.registration_user.SingActivity
 
 class Main_Window_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_window)
+        val BtnLogOut: Button = findViewById(R.id.btnLogOut)
+
+        BtnLogOut.setOnClickListener{
+            val intent = Intent(this,SingActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
